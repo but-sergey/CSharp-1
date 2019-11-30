e99e762036697290d1af1264131337879d6d242c
but-sergey <but-sergey@bk.ru>
2019-11-30 21:22:28 +0300

    Задание 4 выполнено

--- HW-2/Task04/Program.cs
+++ HW-2/Task04/Program.cs
@@ -20,8 +20,61 @@ namespace Task04
 {
     class Program
     {
+        static bool Check(string user, string pass)
+        {
+            return ((user == "root") && (pass == "GeekBrains"));
+        }
+
+        static string getAttemptsString(int num)
+        {
+            switch (num)
+            {
+                case 1:
+                    return $"Осталась {num} попытка";
+                case 2:
+                case 3:
+                case 4:
+                    return $"Осталось {num} попытки";
+                default:
+                    return $"Осталось {num} попыток";
+
+            }
+        }
+
         static void Main(string[] args)
         {
+            string user = "";
+            string pass = "";
+            
+            int count = 0;
+            int maxCount = 3;
+
+            do
+            {
+                count++;
+                
+                Console.Clear();
+                
+                if (count == 1)
+                    Console.WriteLine($"Введите данные для входа. {getAttemptsString(maxCount)}");
+                else
+                    Console.WriteLine($"Данные не верны. Повторите ввод. {getAttemptsString(maxCount - count + 1)}");
+
+                Console.Write("Пользователь: ");
+                user = Console.ReadLine();
+                Console.Write("Пароль: ");
+                pass = Console.ReadLine();
+
+                if (count == maxCount) break;
+
+            } while (! Check(user, pass));
+
+            if (Check(user, pass))
+                Console.WriteLine("Доступ разрешен!");
+            else
+                Console.WriteLine("ДОСТУП ЗАПРЕЩЕН!!!");
+
+            Console.ReadLine();
         }
     }
 }
