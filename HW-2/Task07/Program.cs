975d08337de44c95eb65919e8cfdd0257b0ff216
but-sergey <but-sergey@bk.ru>
2019-12-01 17:48:55 +0300

    Задание 7 выполнено

--- HW-2/Task07/Program.cs
+++ HW-2/Task07/Program.cs
@@ -17,8 +17,54 @@ namespace Task07
 {
     class Program
     {
+        static void Print(int min, int max)
+        {
+            if (min == max)
+            {
+                Console.WriteLine($"{min}.");
+            }
+            else if (min > max)
+            {
+                Console.WriteLine($"Ошибка в параметрах, {min} > {max}!");
+            }
+            else
+            {
+                Console.Write($"{min}, ");
+                Print(min + 1, max);
+            }
+        }
+
+        // рекурсивный подсчет суммы сделал без использования внешней переменной
+        static int CalcSum(int min, int max, int sum = 0)
+        {
+            if (min == max)
+            {
+                return sum + min;
+            }
+            else if (min > max)
+            {
+                Console.WriteLine($"Ошибка в параметрах, {min} > {max}!");
+                return 0;
+            }
+            else
+            {
+                return min + CalcSum(min + 1, max, sum);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Список чисел от {a} до {b}: ");
+            Print(a, b);
+
+            Console.WriteLine($"Сумма чисел от {a} до {b} равна {CalcSum(a, b)}");
+
+            Console.ReadLine();
         }
     }
 }
