6a584d9f192d2154d99210619b93388f92cd0ef0
but-sergey <but-sergey@bk.ru>
2019-12-04 14:42:07 +0300

    Задание 2 выполнено

--- HW-3/Task02/Program.cs
+++ HW-3/Task02/Program.cs
@@ -5,7 +5,7 @@
 // === Задание 02 ===
 // а) С клавиатуры вводятся числа, пока не будет введен 0
 // (каждое число в новой строке). Требуется подсчитать сумму
-// всех нечетных положительных чисел.Сами числа и сумму вывести
+// всех нечетных положительных чисел. Сами числа и сумму вывести
 // на экран, используя tryParse;
 // б) Добавить обработку исключительных ситуаций на то, что могут
 // быть введены некорректные данные.При возникновении ошибки
@@ -22,8 +22,51 @@ namespace Task02
 {
     class Program
     {
+        static int TryInput()
+        {
+            int num;
+            bool flag;
+
+            do
+            {
+                flag = int.TryParse(Console.ReadLine(), out num);
+                if (!flag)
+                {
+                    Console.WriteLine("Неверный ввод!");
+                }
+            } while (!flag);
+
+            return num;
+        }
+
         static void Main(string[] args)
         {
+            int num = 0;
+            int sum = 0;
+            string numbers = "";
+
+            Console.WriteLine("Введите числа (0 - для окончания ввода)");
+            do
+            {
+                num = TryInput();
+                if ((num > 0) && (num % 2 != 0))
+                {
+                    sum += num;
+                    if(numbers.Length == 0)
+                    {
+                        numbers = num.ToString();
+                    }
+                    else
+                    {
+                        numbers += $", {num}";
+                    }
+                }
+            } while (num != 0);
+
+            Console.WriteLine("Введены следующие нечетные положительные числа: " + numbers);
+            Console.WriteLine($"Их сумма равна {sum}");
+            
+            Console.ReadLine();
         }
     }
 }
