61d26ec51b79c57c1e560b74a2a4863f9c9c655f
but-sergey <but-sergey@bk.ru>
2019-12-04 08:58:12 +0300

    Initial commit

---
+++ HW-3/Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 2
+// Бут Сергей
+//
+// === Задание 01 ===
+// а) Дописать структуру Complex, добавив метод вычитания
+// комплексных чисел.Продемонстрировать работу структуры;
+// б) Дописать класс Complex, добавив методы вычитания и
+// произведения чисел.Проверить работу класса;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
