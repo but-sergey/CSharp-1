dd8c5845a869117ff3bc397af787da21594e694b
but-sergey <but-sergey@bk.ru>
2019-12-07 21:39:10 +0300

    Initial HW-4 commit

---
+++ HW-4/Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 4
+// Бут Сергей
+//
+// === Задание 04 ===
+//
+// Решить задачу с логинами из урока 2, только логины и пароли считать из файла в
+// массив.Создайте структуру Account, содержащую Login и Password.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
