05fe5f299dc33c4e7daa1b37d242cf19c58df47c
but-sergey <but-sergey@bk.ru>
2019-12-09 20:34:41 +0300

    Задание 4 выполнено

--- HW-4/Task04/Program.cs
+++ HW-4/Task04/Program.cs
@@ -13,13 +13,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task04
 {
+    public struct Account
+    {
+        public string Login;
+        public string Password;
+    }
+
     class Program
     {
+        static bool Check(Account UserInfo)
+        {
+            return ((UserInfo.Login == "root") && (UserInfo.Password == "GeekBrains"));
+        }
+
         static void Main(string[] args)
         {
+            string FileName = "users.txt";
+            if (!File.Exists(FileName))
+            {
+                Console.WriteLine($"Файл {FileName} не найден!");
+            }
+            else
+            {
+                string[] FileLines = File.ReadAllLines(FileName);
+                Account[] Users = new Account[FileLines.Length];
+                
+                for (int i = 0; 2*i+1 < FileLines.Length; i++)
+                {
+                    Users[i].Login = FileLines[2 * i];
+                    Users[i].Password = FileLines[2 * i + 1];
+
+                    Console.WriteLine($"Пользователь {Users[i].Login}/{Users[i].Password} - доступ {(Check(Users[i]) ? "разрешен" : "запрещен")}");
+                }
+            }
+            
+            Console.ReadLine();
         }
     }
 }
