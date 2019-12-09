2fe330fac89e1ae744c33ffd1461c717710139e0
but-sergey <but-sergey@bk.ru>
2019-12-09 11:49:07 +0300

    Задание 2-а выполнено

--- HW-4/Task02/Program.cs
+++ HW-4/Task02/Program.cs
@@ -18,10 +18,89 @@ using System.Threading.Tasks;
 
 namespace Task02
 {
+    static class ThreeCount
+    {
+        static public int ArrCountThree(int[] arr)
+        {
+            int counter = 0;
+            
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int sumdiv = 0;
+
+                sumdiv += (arr[i - 1] % 3 == 0) ? 1 : 0;
+                sumdiv += (arr[i] % 3 == 0) ? 1 : 0;
+
+                if (sumdiv == 1)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static public int ArrCountThree(int[] arr, ref int[] pairs)
+        {
+            int counter = ArrCountThree(arr);
+
+            Array.Resize(ref pairs, counter);
+
+            counter = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int sumdiv = 0;
+
+                sumdiv += (arr[i - 1] % 3 == 0) ? 1 : 0;
+                sumdiv += (arr[i] % 3 == 0) ? 1 : 0;
+
+                if (sumdiv == 1)
+                {
+                    pairs[counter] = i - 1;
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int MinValue = -10000;
+            int MaxValue = 10000;
+            int[] arr = new int[20];
+            var rand = new Random();
+            int counter = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(MinValue, MaxValue);
+            }
+
+            // Вариант 1 - без вывода самих пар
+            counter = ThreeCount.ArrCountThree(arr);
+            Console.WriteLine($"Вариант вызова без списка пар\nКоличество найденных пар: {counter}\n");
+
+            // Вариант 2 - с выводом пар
+            int[] pairs = new int[0];
+            counter = ThreeCount.ArrCountThree(arr, ref pairs);
+            Console.WriteLine($"Вариант вызова со списком пар\nКоличество найденных пар: {counter}");
+            for(int i = 0; i < pairs.Length; i++)
+            {
+                Console.WriteLine($"Пара №{i+1}: {arr[i]}, {arr[i+1]}.");
+            }
+
+            Console.Write("\nИсходный массив: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($" {arr[i]}{((i == arr.Length - 1) ? "." : ",")}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
