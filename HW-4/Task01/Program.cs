affc74a6c386e2ba8000aedb43957976d5d22bb3
but-sergey <but-sergey@bk.ru>
2019-12-09 10:45:29 +0300

    Задание 1 выполнено

--- HW-4/Task01/Program.cs
+++ HW-4/Task01/Program.cs
@@ -24,6 +24,37 @@ namespace Task01
     {
         static void Main(string[] args)
         {
+            int MinValue = -10000;
+            int MaxValue = 10000;
+            int[] arr = new int[20];
+            var rand = new Random();
+            int counter = 0;
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(MinValue, MaxValue);
+
+                if (i != 0)
+                {
+                    int sumdiv = 0;
+                    sumdiv += (arr[i - 1] % 3 == 0) ? 1 : 0;
+                    sumdiv += (arr[i] % 3 == 0) ? 1 : 0;
+                    if (sumdiv == 1)
+                    {
+                        counter++;
+                        Console.WriteLine($"Пара №{counter}: {arr[i - 1]}, {arr[i]}.");
+                    }
+                }
+            }
+            Console.WriteLine($"\nИтого {counter} пар.\n");
+
+            Console.Write("Исходный массив:");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($" {arr[i]}{((i == arr.Length - 1) ? "." : ",")}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
