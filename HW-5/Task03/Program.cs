ea1e4692f28d16dc721afc44624ab209c6de8d5b
but-sergey <but-sergey@bk.ru>
2019-12-11 16:17:20 +0300

    Initial HW-5 commit

---
+++ HW-5/Task03/Program.cs
@@ -0,0 +1,28 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 5
+// Бут Сергей
+//
+// === Задание 03 ===
+//
+// * Для двух строк написать метод, определяющий, является ли одна строка перестановкой
+// другой.Регистр можно не учитывать:
+// а) с использованием методов C#;
+// б) * разработав собственный алгоритм.
+// Например: badc являются перестановкой abcd.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
