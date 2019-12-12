4a9b27b7003b3cfabef4bd963484f2d7360a2325
but-sergey <but-sergey@bk.ru>
2019-12-12 10:26:25 +0300

    Задание 1 выполнено

--- HW-5/Task01/Program.cs
+++ HW-5/Task01/Program.cs
@@ -16,13 +16,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Task01
 {
     class Program
     {
+        static bool CheckWithoutRegex(string login)
+        {
+            bool flag = true;
+            int MaxLength = 10;
+            int MinLength = 2;
+            
+            // Проверяем длину логина
+            if ((login.Length >= MinLength) && (login.Length <= MaxLength))
+            {
+                // Проверяем первый символ
+                char ch = login[0];
+                if (((ch >= 'a') && (ch <= 'z')) ||
+                    ((ch >= 'A') && (ch <= 'Z')))
+                {
+                    // Проверяем последующие символы
+                    for (int i = 1; i < login.Length; i++)
+                    {
+                        ch = login[i];
+                        if (!(((ch >= 'a') && (ch <= 'z')) ||
+                              ((ch >= 'A') && (ch <= 'Z')) ||
+                              ((ch >= '0') && (ch <= '9'))))
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                else { flag = false; }
+            }
+            else { flag = false; }
+
+            return flag;
+        }
+
+        static bool CheckWithRegex(string login)
+        {
+            Regex MyRegex = new Regex(@"^[a-zA-Z][a-zA-Z0-9]{1,9}$");
+            return MyRegex.IsMatch(login);
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Проверка без использования регулярных выражений: ");
+            Console.WriteLine($"Логин {(CheckWithoutRegex(login) ? "" : "не ")}корректный.");
+
+            Console.Write("Проверка c использованием регулярных выражений: ");
+            Console.WriteLine($"Логин {(CheckWithRegex(login) ? "" : "не ")}корректный.");
+
+            Console.ReadLine();
         }
     }
 }
