a957864116de6aaabd89950c90a9af5ee6040530
but-sergey <but-sergey@bk.ru>
2019-12-12 16:46:22 +0300

    Задание 3 выполнено

--- HW-5/Task03/Program.cs
+++ HW-5/Task03/Program.cs
@@ -21,8 +21,56 @@ namespace Task03
 {
     class Program
     {
+        static bool IsInversion(string Str1, string Str2)
+        {
+            bool Flag = true;
+            if(Str1.Length == 0 || Str1.Length != Str2.Length)
+            {
+                Flag = false;
+            }
+            else
+            {
+                for(int i = 0; i < Str1.Length; i++)
+                {
+                    int Ind = Str2.IndexOf(Str1[i]);
+                    if(Ind == -1)
+                    {
+                        Flag = false;
+                        break;
+                    }
+                    else
+                    {
+                        Str2.Remove(Ind, 1);
+                    }
+                }
+            }
+            return Flag;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Введите первую строку: ");
+            string Str1 = Console.ReadLine();
+
+            Console.Write("Введите вторую строку: ");
+            string Str2 = Console.ReadLine();
+
+            Console.WriteLine("Вариант 1");
+            
+            Console.WriteLine($"Введеные строки {(IsInversion(Str1, Str2) ? "" : "не ")}являются перестановками");
+
+
+            Console.WriteLine("Вариант 2");
+
+            char[] CharArr1 = Str1.ToCharArray();
+            char[] CharArr2 = Str2.ToCharArray();
+            Array.Sort(CharArr1);
+            Array.Sort(CharArr2);
+            bool isInv = Enumerable.SequenceEqual(CharArr1, CharArr2);
+            
+            Console.WriteLine($"Введеные строки {(isInv ? "" : "не ")}являются перестановками");
+            
+            Console.ReadLine();
         }
     }
 }
