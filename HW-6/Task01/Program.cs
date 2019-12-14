c4f06613894df492960232ed60a459e8734c5ecd
but-sergey <but-sergey@bk.ru>
2019-12-14 14:08:20 +0300

    Initial HW-6 commit

---
+++ HW-6/Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 6
+// Бут Сергей
+//
+// === Задание 01 ===
+//
+// Изменить программу вывода таблицы функции так, чтобы можно было передавать
+// функции типа double (double, double). Продемонстрировать работу на функции
+// с функцией a* x^2 и функцией a* sin(x).
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
