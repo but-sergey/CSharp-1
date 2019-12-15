70e486257c09b9a6b8858f7c2a53694dde1b6d68
but-sergey <but-sergey@bk.ru>
2019-12-15 15:07:51 +0300

    Задание 1 выполнено

--- HW-6/Task01/Program.cs
+++ HW-6/Task01/Program.cs
@@ -17,10 +17,41 @@ using System.Threading.Tasks;
 
 namespace Task01
 {
+    public delegate double Fun(double a, double x);
+
     class Program
     {
+        public static void Table2(Fun F, double a, double MaxA, double x, double MaxX)
+        {
+            double ta = a;
+            Console.WriteLine("----- A -------- X ------- Y -----");
+            while (ta <= MaxA)
+            {
+                double tx = x;
+                while (tx <= MaxX)
+                {
+                    Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", ta, tx, F(ta, tx));
+                    tx++;
+                }
+                ta++;
+            }
+            Console.WriteLine("----------------------------------");
+        }
+
+        public static double MySin(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Таблица функции a*x^2:");
+            Table2(delegate (double a, double x) { return a * x * x; }, -2, 2, -2, 2);
+
+            Console.WriteLine("Таблица функции a * sin(x):");
+            Table2(MySin, -2, 2, -2, 2);
+
+            Console.ReadLine();
         }
     }
 }
