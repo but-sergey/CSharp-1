3b45828be5f0f12c3c975ed340784022aa29d0d3
but-sergey <but-sergey@bk.ru>
2019-12-21 12:11:01 +0300

    Initial HW-8 commit

---
+++ HW-8/Task01/Program.cs
@@ -0,0 +1,24 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 8
+// Бут Сергей
+//
+// === Задание 01 ===
+//
+// С помощью рефлексии выведите все свойства структуры DateTime
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HW-8/Task02/Form1.cs
@@ -0,0 +1,30 @@
+﻿// С#. Уровень 1
+// Домашнее задание к уроку 8
+// Бут Сергей
+//
+// === Задание 02 ===
+//
+// Создайте простую форму на котором свяжите свойство Text элемента
+// TextBox со свойством Value элемента NumericUpDown
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
